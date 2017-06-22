d28b2a49f64b48fdd58fbf8aad4fd279030cbdae
Owen Essen <owenessen@guest-2-00532.dhcp.unc.edu>
2017-06-21 21:18:38 -0400

    Preping document for AJAX call.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,37 +18,43 @@ $( document ).ready(function() {
 	$( "#searchFifteen" ).click(function() {
   		resultsRequested = 15;
 	});
+
+	// This click event should contain the AJAX call. 
+	$( "#searchBtn" ).click(function() {
+
+		// ADD AJAX CALL HERE
+
+
+		//Place this functionality inside search button click event. 
+
+		// This for loop then generates the correct number of article thumbnails
+		for (var i = 0; i < resultsRequested; i++){
+
+			// Set these variables equal to the different values found in Curtis's AJAX call. 
+			var articleTitle;
+			var articleAuthor;
+			var articleSection;
+			var articleTimePublished;
+			var articleLink;
+
+			// This part then generates the necesary HTML elements. 
+			var articleThumbnail = $('div');
+			var thumbnailTitle = $('h2').text(articleTitle);
+			articleThumbnail.append(thumbnailTitle);
+			var thumbnailAuthor = $('p').text(articleAuthor);
+			articleThumbnail.append(thumbnailAuthor);
+			var thumbnailSection = $('p').text(articleSection);
+			articleThumbnail.append(thumbnailSection);
+			var thumbnailTimePublished = $('p').text(articleTimePublished);
+			articleThumbnail.append(thumbnailTimePublished);
+			var thumbnailLink = $('link').text(articleLink);
+			thumbnailLink.attr('href', articleLink);
+			articleThumbnail.append(thumbnailLink);
+
+			// ADD CODE HERE TO APPEND articleThumbnail TO CHRIS'S HOLDER DIV
+			$('#articlePanel').append(articleThumbnail);
+			
+		};
+	});
+
 });
-
-
-
-//Place this functionality inside search button click event. 
-
-	// This for loop then generates the correct number of article thumbnails
-	for (var i = 0; i < resultsRequested; i++){
-
-		// Set these variables equal to the different values found in Curtis's AJAX call. 
-		var articleTitle;
-		var articleAuthor;
-		var articleSection;
-		var articleTimePublished;
-		var articleLink;
-
-		// This part then generates the necesary HTML elements. 
-		var articleThumbnail = $('div');
-		var thumbnailTitle = $('h2').text(articleTitle);
-		articleThumbnail.append(thumbnailTitle);
-		var thumbnailAuthor = $('p').text(articleAuthor);
-		articleThumbnail.append(thumbnailAuthor);
-		var thumbnailSection = $('p').text(articleSection);
-		articleThumbnail.append(thumbnailSection);
-		var thumbnailTimePublished = $('p').text(articleTimePublished);
-		articleThumbnail.append(thumbnailTimePublished);
-		var thumbnailLink = $('link').text(articleLink);
-		thumbnailLink.attr('href', articleLink);
-		articleThumbnail.append(thumbnailLink);
-
-		// ADD CODE HERE TO APPEND articleThumbnail TO CHRIS'S HOLDER DIV
-
-		
-	};
